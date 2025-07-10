72f3e7bf5a19e6911eb4b4c623b8d341e0bf8bff
Guillaume Royer <groy@fiveonefour.com>
2025-07-10 11:46:27 -0400

    fix bundler tsconfig issue.

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,21 +1,21 @@
 // Export utilities
-export * from "./lib/utils.js";
+export * from "./lib/utils";
 
 // Export components
-export * from "./components/button.js";
-export * from "./components/card.js";
-export * from "./components/input.js";
-export * from "./components/label.js";
-export * from "./components/textarea.js";
-export * from "./components/form.js";
-export * from "./components/dialog.js";
-export * from "./components/table.js";
-export * from "./components/badge.js";
-export * from "./components/select.js";
+export * from "./components/button";
+export * from "./components/card";
+export * from "./components/input";
+export * from "./components/label";
+export * from "./components/textarea";
+export * from "./components/form";
+export * from "./components/dialog";
+export * from "./components/table";
+export * from "./components/badge";
+export * from "./components/select";
 
 // Export theme components
-export * from "./components/theme-provider.js";
-export * from "./components/theme-toggle.js";
+export * from "./components/theme-provider";
+export * from "./components/theme-toggle";
 
 // Export hooks
 // Hooks will be added here as they are created
