fbb78602716f3b3e011301aec47c84a84110bb9e
groy-514 <groy@fiveonefour.com>
2025-08-04 09:42:03 -0400

    Groy/eng 522 host the elastic search backend fastify and system (#128)
    * updated the elastic client to support api key from env vars
    
    * [wip] elastic prod. package/models now a compiled package.
    
    * fixed pnpm lock merge
    
    * updated lockfile to latest
    
    * compiler plugin change
    
    * reverted the models package compilation
    
    * added tsup
    
    * removed unecessary step

--- ufa/services/retrieval-elasticsearch-foobar/src/elasticsearch/client.ts
+++ ufa/services/retrieval-elasticsearch-foobar/src/elasticsearch/client.ts
@@ -3,6 +3,14 @@ import { Client } from "@elastic/elasticsearch";
 // Create Elasticsearch client instance
 export const esClient = new Client({
   node: process.env.ELASTICSEARCH_URL || "http://localhost:9200",
+
+  // Authentication required in production (Elastic Cloud)
+  ...(process.env.NODE_ENV === "production" && {
+    auth: {
+      apiKey: process.env.ELASTICSEARCH_API_KEY || "",
+    },
+  }),
+
   maxRetries: 5,
   requestTimeout: 60000,
   sniffOnStart: false,
